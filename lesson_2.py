bdc4b712695ddc7e837f209384b6864b538c9ddb
Igor <igglike@gmail.com>
2025-12-30 18:53:22 +0600

    Добавил пример с наследованием

---
+++ lesson_2.py
@@ -0,0 +1,25 @@
+# родительский, суперкласс
+class Car:
+    # ининциализатор
+    def __init__(self,  model, color):
+        self.model = model
+        self.color = color
+        self.speed = 0
+
+    def drive_to(self, destination):
+        print(f"Car {self.model} Driving to", destination)
+
+# дочерний, подклас
+class Bus(Car):
+    def __init__(self, model, color, number):
+        super().__init__(model, color)
+        self.number = number
+
+    def drive_to(self, destination):
+        super().drive_to(destination)
+        print(f"Bus {self.number} Driving to", destination)
+
+bus_42 = Bus("Ikarus", "green", 42)
+print(bus_42.number)
+print(bus_42.speed)
+bus_42.drive_to("Bishkek")
